714f2e5d031c110d1430dbde5c40f9d029e96e2c
rosyvs <rosy.southwell@gmail.com>
2022-10-05 17:14:15 +0000

    manifestos for ISAT-SI

--- prep_scripts/prepare_isatsi.py
+++ prep_scripts/prepare_isatsi.py
@@ -1,11 +1,10 @@
-import glob
 import os
-import wave
 import csv
 from tqdm import tqdm
 from prep_utils import split_to_chunks
 import numpy as np
-# TODO: reduce/rename cols in METADATA csv if needed
+import pandas as pd
+import math
 
 CORPORA_PATH = '/mnt/shared/CORPORA/'
 CORPUS_DIR = 'ISAT-SI/' 
@@ -20,11 +19,19 @@ os.makedirs(os.path.join(CORPORA_PATH,'data_manifests'), exist_ok=True)
 for split in splits:
     csv_file = os.path.join(CORPORA_PATH,'data_manifests',f'ISAT-SI_{split}.csv')
 
-# TODO: just read the metadata.csv and work from that
-    metadatafile = #TODO
-    for row in metadatafile: #TODO
-        if duration_sec>CHUNK_SEC:
-            #print(f'Long file: splitting into {math.ceil(duration_sec/CHUNK_SEC)} segments of <={CHUNK_SEC} seconds')
+    metadatafile = os.path.join(CORPORA_PATH, CORPUS_DIR, split, 'METADATA.csv')
+    metadata = pd.read_csv(metadatafile)
+    print(f'{len(metadata)} .wav files in split "{split}". Collecting metadata for csv...')
+
+    csv_output = [["ID", "start", "end", "duration", "speaker", "filepath"]]
+
+    for i,row in metadata.iterrows(): #TODO
+        duration = row['duration']
+        ID = row['ID']
+        speaker = row['speaker']
+        wav_file = row['filepath']
+        if row['duration_sec']>CHUNK_SEC:
+            print(f'Long file: splitting into {math.ceil(duration_sec/CHUNK_SEC)} segments of <={CHUNK_SEC} seconds')
             chunks = split_to_chunks(CHUNK_SEC, duration_sec, SRATE)
             csv_line = [[
                 ID,
@@ -34,7 +41,7 @@ for split in splits:
                 speaker,
                 wav_file
             ] for c in chunks]
-            entry.extend(csv_line)
+            csv_output.extend(csv_line)
 
         else:
             start=0
@@ -48,8 +55,7 @@ for split in splits:
                 speaker,
                 wav_file
             ]
-            entry.append(csv_line)
-        csv_output = csv_output+entry
+            csv_output.append(csv_line)
 
     # Writing the csv lines
     with open(csv_file, mode="w") as csv_f:
@@ -68,118 +74,118 @@ for split in splits:
 
 
 
-
+# TODO: TODO: TODO: TODO: TODO: TODO: TODO: TODO: TODO: TODO: TODO: TODO: TODO: TODO: TODO: TODO: 
 # TODO: make verifiaction pairs file from TEST set
 
-# from voxceleb_prepare.py:
+# # from voxceleb_prepare.py:
 
-def prepare_csv_enrol_test(data_folders, save_folder, verification_pairs_file):
-    """
-    Creates the csv file for test data (useful for verification)
+# def prepare_csv_enrol_test(data_folders, save_folder, verification_pairs_file):
+#     """
+#     Creates the csv file for test data (useful for verification)
 
-    Arguments
-    ---------
-    data_folder : str
-        Path of the data folders
-    save_folder : str
-        The directory where to store the csv files.
+#     Arguments
+#     ---------
+#     data_folder : str
+#         Path of the data folders
+#     save_folder : str
+#         The directory where to store the csv files.
 
-    Returns
-    -------
-    None
-    """
+#     Returns
+#     -------
+#     None
+#     """
 
-    # msg = '\t"Creating csv lists in  %s..."' % (csv_file)
-    # logger.debug(msg)
+#     # msg = '\t"Creating csv lists in  %s..."' % (csv_file)
+#     # logger.debug(msg)
 
-    csv_output_head = [
-        ["ID", "duration", "wav", "start", "stop", "spk_id"]
-    ]  # noqa E231
+#     csv_output_head = [
+#         ["ID", "duration", "wav", "start", "stop", "spk_id"]
+#     ]  # noqa E231
 
-    for data_folder in data_folders:
+#     for data_folder in data_folders:
 
-        test_lst_file = verification_pairs_file
+#         test_lst_file = verification_pairs_file
 
-        enrol_ids, test_ids = [], []
+#         enrol_ids, test_ids = [], []
 
-        # Get unique ids (enrol and test utterances)
-        for line in open(test_lst_file):
-            e_id = line.split(" ")[1].rstrip().split(".")[0].strip()
-            t_id = line.split(" ")[2].rstrip().split(".")[0].strip()
-            enrol_ids.append(e_id)
-            test_ids.append(t_id)
+#         # Get unique ids (enrol and test utterances)
+#         for line in open(test_lst_file):
+#             e_id = line.split(" ")[1].rstrip().split(".")[0].strip()
+#             t_id = line.split(" ")[2].rstrip().split(".")[0].strip()
+#             enrol_ids.append(e_id)
+#             test_ids.append(t_id)
 
-        enrol_ids = list(np.unique(np.array(enrol_ids)))
-        test_ids = list(np.unique(np.array(test_ids)))
+#         enrol_ids = list(np.unique(np.array(enrol_ids)))
+#         test_ids = list(np.unique(np.array(test_ids)))
 
-        # Prepare enrol csv
-        logger.info("preparing enrol csv")
-        enrol_csv = []
-        for id in enrol_ids:
-            wav = data_folder + "/wav/" + id + ".wav"
+#         # Prepare enrol csv
+#         logger.info("preparing enrol csv")
+#         enrol_csv = []
+#         for id in enrol_ids:
+#             wav = data_folder + "/wav/" + id + ".wav"
 
-            # Reading the signal (to retrieve duration in seconds)
-            signal, fs = torchaudio.load(wav)
-            signal = signal.squeeze(0)
-            audio_duration = signal.shape[0] / SAMPLERATE
-            start_sample = 0
-            stop_sample = signal.shape[0]
-            [spk_id, sess_id, utt_id] = wav.split("/")[-3:]
+#             # Reading the signal (to retrieve duration in seconds)
+#             signal, fs = torchaudio.load(wav)
+#             signal = signal.squeeze(0)
+#             audio_duration = signal.shape[0] / SAMPLERATE
+#             start_sample = 0
+#             stop_sample = signal.shape[0]
+#             [spk_id, sess_id, utt_id] = wav.split("/")[-3:]
 
-            csv_line = [
-                id,
-                audio_duration,
-                wav,
-                start_sample,
-                stop_sample,
-                spk_id,
-            ]
+#             csv_line = [
+#                 id,
+#                 audio_duration,
+#                 wav,
+#                 start_sample,
+#                 stop_sample,
+#                 spk_id,
+#             ]
 
-            enrol_csv.append(csv_line)
+#             enrol_csv.append(csv_line)
 
-        csv_output = csv_output_head + enrol_csv
-        csv_file = os.path.join(save_folder, ENROL_CSV)
+#         csv_output = csv_output_head + enrol_csv
+#         csv_file = os.path.join(save_folder, ENROL_CSV)
 
-        # Writing the csv lines
-        with open(csv_file, mode="w") as csv_f:
-            csv_writer = csv.writer(
-                csv_f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
-            )
-            for line in csv_output:
-                csv_writer.writerow(line)
+#         # Writing the csv lines
+#         with open(csv_file, mode="w") as csv_f:
+#             csv_writer = csv.writer(
+#                 csv_f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+#             )
+#             for line in csv_output:
+#                 csv_writer.writerow(line)
 
-        # Prepare test csv
-        logger.info("preparing test csv")
-        test_csv = []
-        for id in test_ids:
-            wav = data_folder + "/wav/" + id + ".wav"
+#         # Prepare test csv
+#         logger.info("preparing test csv")
+#         test_csv = []
+#         for id in test_ids:
+#             wav = data_folder + "/wav/" + id + ".wav"
 
-            # Reading the signal (to retrieve duration in seconds)
-            signal, fs = torchaudio.load(wav)
-            signal = signal.squeeze(0)
-            audio_duration = signal.shape[0] / SAMPLERATE
-            start_sample = 0
-            stop_sample = signal.shape[0]
-            [spk_id, sess_id, utt_id] = wav.split("/")[-3:]
+#             # Reading the signal (to retrieve duration in seconds)
+#             signal, fs = torchaudio.load(wav)
+#             signal = signal.squeeze(0)
+#             audio_duration = signal.shape[0] / SAMPLERATE
+#             start_sample = 0
+#             stop_sample = signal.shape[0]
+#             [spk_id, sess_id, utt_id] = wav.split("/")[-3:]
 
-            csv_line = [
-                id,
-                audio_duration,
-                wav,
-                start_sample,
-                stop_sample,
-                spk_id,
-            ]
+#             csv_line = [
+#                 id,
+#                 audio_duration,
+#                 wav,
+#                 start_sample,
+#                 stop_sample,
+#                 spk_id,
+#             ]
 
-            test_csv.append(csv_line)
+#             test_csv.append(csv_line)
 
-        csv_output = csv_output_head + test_csv
-        csv_file = os.path.join(save_folder, TEST_CSV)
+#         csv_output = csv_output_head + test_csv
+#         csv_file = os.path.join(save_folder, TEST_CSV)
 
-        # Writing the csv lines
-        with open(csv_file, mode="w") as csv_f:
-            csv_writer = csv.writer(
-                csv_f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
-            )
-            for line in csv_output:
-                csv_writer.writerow(line)
+#         # Writing the csv lines
+#         with open(csv_file, mode="w") as csv_f:
+#             csv_writer = csv.writer(
+#                 csv_f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+#             )
+#             for line in csv_output:
+#                 csv_writer.writerow(line)
